7ab06338c1397b3ac06cf12d7c0e5d86df4ddd6e
Viktor Alexandersson <viktoralexandersson@live.se>
2025-09-19 10:27:00 +0200

    Uppdaterad mqtt

--- code/mqtt_tx.py
+++ code/mqtt_tx.py
@@ -4,13 +4,14 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client()
 
 # Anslut till brokern
-client.connect("test.mosquitto.org", 1883, 60)
+client.username_pw_set("elektronik", "elektronik")
+client.connect("100.82.0.4", 1883, 60)
 
 # Starta loop för att hålla anslutningen igång
 client.loop_start()
 
 # Skicka ett meddelande till specificerad topic
-result = client.publish("ei24/viktor", "san")
+result = client.publish("pi10/sensor/1", 70)
 
 # Vänta tills meddelandet är skickat
 result.wait_for_publish()
